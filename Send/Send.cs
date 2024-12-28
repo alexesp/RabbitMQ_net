5d7f77f15d9800009708c3706c51945fc70a09c9
alexesp <alexizaes@hotmail.com>
2024-12-28 01:48:16 +0100

    first commit

---
+++ Send/Send.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using RabbitMQ.Client;
+
+
+   
+        var factory = new ConnectionFactory() {HostName = "localhost"};
+        //Console.Writeline("Send message!");
+        using var connection = await factory.CreateConnectionAsync();
+     
+            using var channel = await connection.CreateChannelAsync();
+           
+               await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive:false,autoDelete:false,arguments:null);
+                string message = "Hola Alex from RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+               await channel.BasicPublishAsync(exchange: string.Empty,routingKey: "hello", body:body);
+                Console.WriteLine($"[x] Sent {message}");
+        
+ 
+        Console.WriteLine("Press anty key to ext....");
+        Console.ReadLine();
+    
